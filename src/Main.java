93998156c5b75f4803d0cd097ae9679eadf8da01
ZhangJiachen13 <1518048969@qq.com>
2025-02-20 17:08:41 +0800

    my first commit

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,13 @@ public class Main{
 
         System.out.println("Now playing the song");
         mbox.playSong(mbox.getSongID(), mbox.getPremiumSong(), 2);
-        
-        // --- TODO
+       
+        MusicBox mbox1 = new MusicBox("S34TG65", 'N', "Raindrops", "Misty", 
+        "The path less traveled", "Country", "BZEE Music", "Rhythm Divine");
+
+        System.out.println("Now playing the song");
+        mbox1.playSong(mbox.getSongID(), mbox.getPremiumSong(), 1);
+        // --- 
 
         /* Repeat the above code to create another object  
          * Pass different parameters to the MusicBox constructor, but initialise premiumSong to N and when calling the
--- src/MusicBox.java
+++ src/MusicBox.java
@@ -17,8 +17,13 @@ public class MusicBox {
         this.songID = songID;
         this.isSongPremium = premiumSong;
         this.songArtists = artists;
+        this.songTitle = title;
+        this.songAlbum = Album;
+        this.songGenre = genre;
+        this.songProducer = producer;
+        this.songMusicLabel = label;
         
-        // -- TODO
+        
         /* Initialise  the rest of the variables in this Constructor block*/
     }
     
@@ -35,8 +40,28 @@ public class MusicBox {
     public char getPremiumSong() {
         return isSongPremium;
     }
+    public String getSongTitle() {
+        return songTitle;
+    }
+    public String getSongAlbum() {
+        return songAlbum;
+    }
+    public String getSongGenre() {
+        return songGenre;
+    }
+    public String getSongProducer() {
+        return songProducer;
+    }
+    public String getSongMusicLabel() {
+        return songMusicLabel;
+    }
+    public int getNoAds() {
+        return noAds;
+    }
 
-    // -- TODO
+
+
+    // 
         /* Create the rest of "getters" for the remaining variables */
     
         /* The following method is complete. You don't have to do anything here. Just try to understand it. */
